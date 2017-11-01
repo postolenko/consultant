12a69bddf27d3a1cdd9c8406785795d8f5ddfec5
postolenko <denis.postolenko@gmail.com>
2017-11-01 14:18:38 +0200

    Help Tour

--- js/additional_scripts.js
+++ js/additional_scripts.js
@@ -4,39 +4,59 @@
 
 		$(".scroll").mCustomScrollbar();
 
-        // $(".scroll-x").mCustomScrollbar({
-        //     axis:"x"
-        // });
-
 	});
 
 })(jQuery);
 
 $(document).ready(function() {
 
- $(".main-slider").not(".slick-initialized").slick({
-    dots: false,
-    arrows: false,      
-    autoplay: true,
-    autoplaySpeed: 10000,
-    speed: 1200,
-    slidesToShow: 1,
-    fade: true
-  });
+    $(".main-slider").not(".slick-initialized").slick({
+        dots: false,
+        arrows: false,      
+        autoplay: true,
+        autoplaySpeed: 10000,
+        speed: 1200,
+        slidesToShow: 1,
+        fade: true
+    });
 
-  $(".blog-slider").not(".slick-initialized").slick({
-    dots: false,
-    arrows: false,      
-    autoplay: true,
-    autoplaySpeed: 10000,
-    speed: 1200,
-    slidesToShow: 1,
-    fade: true
-  });
+    $(".blog-slider").not(".slick-initialized").slick({
+        dots: false,
+        arrows: false,      
+        autoplay: true,
+        autoplaySpeed: 10000,
+        speed: 1200,
+        slidesToShow: 1,
+        fade: true
+    });
 
 
     $("select").select2({
         minimumResultsForSearch: Infinity
     });
+
+    if( $(".add-rating").length > 0 )  {
+
+        $(".add-rating").rateYo({
+            starWidth: "16px",
+            spacing: "4px",
+            normalFill: "#ffffff",
+            ratedFill: "#eb3745"
+            // rating: rateVal
+        });
+        
+    }
+
+    if( $(".add-rating-big").length > 0 )  {
+
+        $(".add-rating-big").rateYo({
+            starWidth: "29px",
+            spacing: "8px",
+            normalFill: "#ffffff",
+            ratedFill: "#eb3745"
+            // rating: rateVal
+        });
+
+    }
 
 });
